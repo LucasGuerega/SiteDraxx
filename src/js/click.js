356335315f2b7dc45a5a791579d07a15cb439aa1
Lucas Guerega <lucas.g2006@aluno.ifsc.edu.br>
2024-11-07 11:56:37 -0300

    more things

--- src/js/click.js
+++ src/js/click.js
@@ -2,11 +2,20 @@ function portfolio() {
     window.location = "portfolio.html";
 }
 function comission() {
+    window.location = "terms.html";
+}
+function comissionNext() {
     window.location = "comission.html";
 }
 function special() {
     window.location = "special.html";
 }
+function special2() {
+    window.location = "special2.html";
+}
 function back() {
     window.location = "index.html";
+}
+function blue() {
+    window.location = "https://bsky.app/profile/madraxx.bsky.social";
 }
